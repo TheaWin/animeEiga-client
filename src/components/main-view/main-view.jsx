43b5d0e5627d3ac4b049209f22effbdf11aabb40
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-01-05 22:26:13 +0800

    MainView component created and exported

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,12 @@
+//export MainView (main homepage) to be used
+export const MainView = () => {
+  return (
+    <div></div>
+  );
+};
+
+//different export syntax
+/* const MainView = () => {
+  ...
+}
+export MainView; */
