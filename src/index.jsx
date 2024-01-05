59f51e8183e020558ada33f7cc54fbbee2cd11b0
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-01-05 22:27:47 +0800

    import MainView
    imported only MainView component as it will act as the main homepage

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,21 @@
 //Create a React root for displaying content inside a browser DOM element
 import { createRoot } from 'react-dom/client';
 
+/* syntax to import components:
+import { ComponentName } from '[path to the component file]'; */
+import { MainView } from './components/main-view/main-view';
+
 // import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
+
+//Main component
+const AnimeApplication = () => {
+  return <MainView />;
+}
 
 //Finds the root of the app
 const container = document.querySelector("#root");//assigned to the HTML element with the id "root"
 const root = createRoot(container);//establishes the selected HTML element as the root for React app
 
 //instucts React to render the app in the root DOM element
-root.render(<App />);
+root.render(<AnimeApplication />);
