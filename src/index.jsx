4b83c052b3527dc7a8e3a2299a5c3f9037e8b3f6
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-01-05 22:11:11 +0800

    import index.scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 //Create a React root for displaying content inside a browser DOM element
 import { createRoot } from 'react-dom/client';
 
+// import statement to indicate that you need to bundle `./index.scss`
+import "./index.scss";
+
+//Finds the root of the app
 const container = document.querySelector("#root");//assigned to the HTML element with the id "root"
 const root = createRoot(container);//establishes the selected HTML element as the root for React app
 
+//instucts React to render the app in the root DOM element
 root.render(<App />);
