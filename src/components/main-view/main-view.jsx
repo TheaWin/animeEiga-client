cfc480d6f16a593d7e521cb6d6fbeb5c2fc3bd1e
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-01-05 22:41:31 +0800

    import useState

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,13 @@
+//importing useState Hook that allows to track state in a function component
+import { useState } from "react";
+
 //export MainView (main homepage) to be used
 export const MainView = () => {
+  
+  /* using useState to add a state variable to the component with the following syntax:
+    const [currentState, functionUsedToChangeState ] = useState([]); */
+  const [movies, setMovies] = useState([]);
+
   return (
     <div></div>
   );
