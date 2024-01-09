1bb48de91572a05455045bfe43d43e06be869d43
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-01-10 00:03:05 +0800

    add PropTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,6 @@
+//import PropTypes to validates the data types
+import PropTypes from "prop-types";
+
 //extracting the onMovieClick prop
 export const MovieCard = ({movie, onMovieClick}) => {
     return (
@@ -11,4 +14,10 @@ export const MovieCard = ({movie, onMovieClick}) => {
   );
 };
 
-
+//define all the props constraints for the MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Name: PropTypes.string,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
