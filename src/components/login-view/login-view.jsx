35383e8b35e71f0b8ad0df9634e32a187d1f3b26
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-01-10 00:19:51 +0800

    added simple LoginView form

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,16 @@
+//LoginView funciton component
+export const LoginView = () => {
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" />
+      </label>
+      <label>
+        Password:
+        <input type="password" />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+    )
+}
