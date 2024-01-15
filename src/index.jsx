7a2d001a175421bf6946f810bcc999da8f3edc9d
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-01-15 21:12:59 +0800

    import React Bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,23 @@
 //Create a React root for displaying content inside a browser DOM element
 import { createRoot } from 'react-dom/client';
+import Container from "react-bootstrap/Container";
 
 /* syntax to import components:
 import { ComponentName } from '[path to the component file]'; */
 import { MainView } from './components/main-view/main-view';
 
+import "bootstrap/dist/css/bootstrap.min.css"; //hav to be before index.scss
 // import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 //Main component
 const AnimeApplication = () => {
-  return <MainView />;
-}
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
+};
 
 //Finds the root of the app
 const container = document.querySelector("#root");//assigned to the HTML element with the id "root"
