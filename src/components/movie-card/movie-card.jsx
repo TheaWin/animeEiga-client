ed451e3f4014e5a39f551af2fc8f8f3eb9ab54a5
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-01-17 00:39:32 +0800

    incorporated Bootstrap Button & Card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,15 +1,24 @@
 //import PropTypes to validates the data types
 import PropTypes from "prop-types";
+//import Button & Card from Bootstrap
+import { Button, Card } from "react-bootstrap";
 
 //extracting the onMovieClick prop
-export const MovieCard = ({movie, onMovieClick}) => {
+export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     //onClick only works on React elements, e.g. divs, buttons, p's,...
-    <div onClick={() => {
-      onMovieClick(movie);
-    }}>
-    {movie.Name}
-    </div>
+    <Card
+      className="h-100"
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+      style={{ cursor: "pointer" }}
+    >
+      <Card.Img variant="top" src={movie.imageURL} />
+      <Card.Body>
+        <Card.Title>{movie.Name}</Card.Title>
+      </Card.Body>
+    </Card>
   );
 };
 
@@ -18,5 +27,5 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     Name: PropTypes.string,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  onMovieClick: PropTypes.func.isRequired,
 };
